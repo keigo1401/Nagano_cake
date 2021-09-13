9d47f0144825c76fc1b53f96aea07942e480bc5f
keigo1401 <totutotu1015@gmail.com>
2021-09-13 13:18:24 +0000

    [fix]Ruting error解消

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-
+  
+  
+  scope module: 'public' do
   root "homes#top"
   get 'homes/about' => 'homes#about'
   resources :items, only:[:index, :show]
@@ -16,6 +18,7 @@ Rails.application.routes.draw do
   post 'orders/confirm' => 'orders#confirm'
 
   resources :address, only:[:index, :edit, :create, :update, :destroy]
+  end
 
 
   namespace :admin do
@@ -23,9 +26,9 @@ Rails.application.routes.draw do
     resources :items, only:[:index, :new, :create, :show, :edit,:update]
     resources :genres, only:[:index, :create, :edit, :update]
     resources :customers, only:[:index, :show, :edit, :update]
-    get 'admin/orders/:id' => 'admin/orders#show'
-    patch 'admin/orders/:id' => 'admin/orders#update'
-    patch 'admin/order_products/:id' => 'admin/order_products#update'
+    get 'orders/:id' => 'orders#show'
+    patch 'orders/:id' => 'orders#update'
+    patch 'order_products/:id' => 'order_products#update'
   end
 
 
